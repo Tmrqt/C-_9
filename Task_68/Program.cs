ed701b58f8a2e61184c9763c6e7c94a7d00f5c09
Timerkhan <timerkhan12@gmail.com>
2022-12-31 00:18:59 +0500

    done

---
+++ Task_68/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int LeviMikasa(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return LeviMikasa(m - 1, 1);
+    }
+    else
+    {
+        return LeviMikasa(m - 1, LeviMikasa(m, n - 1));
+    }
+}
+
+int ackr = LeviMikasa(m,n);
+System.Console.WriteLine(ackr);
