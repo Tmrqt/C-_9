9b34d92dbaf8056a9577bf230710b6b2afed8e8f
Timerkhan <timerkhan12@gmail.com>
2022-12-31 00:18:44 +0500

    done

---
+++ Task_64/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int p = 1;
+
+int Numbers(int n, int p)
+{
+    if (n == p)
+    {
+        System.Console.Write($"{n};");
+        return n;
+    }
+    else
+    {
+        Numbers(n,p+1);
+        System.Console.Write($"{p};");
+        return p;
+    }
+}
+
+Numbers(n,p);
